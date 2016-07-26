80fd23a74d74f49887106ddb3fb76c6ef62132ee
Andrew R. Edelson <redelso2@san.rr.com>
2016-07-25 18:05:51 -0700

    finished file uploader

---
+++ app/Controller/FilesController.php
@@ -0,0 +1,87 @@
+<?php
+
+class FilesController extends AppController {
+
+  public function initialize() {
+
+    parent::initialize();
+
+    // Include the FlashComponent
+    $this->loadComponent('Flash');
+
+    // Load Files model
+    $this->loadModel('Files');
+
+    // Set the layout
+    $this->layout = 'frontend';
+  }
+
+  public function index() {
+
+    // Load Files model
+    $this->loadModel('Files');
+
+    $uploadData = '';
+
+    if ($this->request->is('post')) {
+
+      if(!empty($this->request->data['File']['file']['name'])) {
+
+        $fileName = $this->request->data['File']['file']['name'];
+        $uploadPath = 'files/';
+        $uploadFile = $uploadPath . $fileName;
+
+        if(move_uploaded_file($this->request->data['File']['file']['tmp_name'], $uploadFile)) {
+          $ipaddress = '';
+
+          if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+              $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+          } else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+              $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+          } else if (isset($_SERVER['HTTP_X_FORWARDED'])) {
+              $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+          } else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+              $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+          } else if (isset($_SERVER['HTTP_FORWARDED'])) {
+              $ipaddress = $_SERVER['HTTP_FORWARDED'];
+          } else if (isset($_SERVER['REMOTE_ADDR'])) {
+              $ipaddress = $_SERVER['REMOTE_ADDR'];
+          } else {
+              $ipaddress = 'UNKNOWN';
+          }
+
+          $uploadData = array(
+              'name' => $fileName,
+              'path' => $uploadPath,
+              'created' => date("Y-m-d H:i:s"),
+              'ip_address' => $ipaddress
+            );
+          $this->Files->save($uploadData);
+
+          if ($this->Files->save($uploadData)) {
+
+            $this->Flash->success(__('File has been uploaded and inserted successfully.'));
+
+          } else {
+
+            $this->Flash->error(__('Unable to upload file, please try again.'));
+          }
+        } else {
+
+          $this->Flash->error(__('Unable to upload file, please try again.'));
+        }
+      } else {
+
+        $this->Flash->error(__('Please choose a file to upload.'));
+      }
+    }
+
+    $this->set('uploadData', $uploadData);
+
+    $files = $this->Files->find('all', ['order' => ['Files.created' => 'DESC']]);
+    $filesRowNum = count($files);
+    $this->set('files', $files);
+    $this->set('filesRowNum', $filesRowNum);
+  }
+
+}
---
+++ app/View/Files/index.ctp
@@ -0,0 +1,34 @@
+<h1>Upload File</h1>
+<div class="content">
+  <?= $this->Flash->render() ?>
+  <div class="upload-frm">
+    <?php echo $this->Form->create($uploadData, ['type' => 'file']); ?>
+    <?php echo $this->Form->input('file', ['type' => 'file', 'class' => 'form-control']); ?>
+    <?php echo $this->Form->button(__('Upload File'), ['type'=>'submit', 'class' => 'form-controlbtn btn-default']); ?>
+    <?php echo $this->Form->end(); ?>
+  </div>
+</div>
+
+<h1>Uploaded Files</h1>
+
+<div class="content">
+  <!-- Table -->
+  <table class="table">
+    <tr>
+      <th width="1%">#</th>
+      <th width="15%">File</th>
+      <th width="5%">Upload Date</th>
+      <th width="5%">IP Address</th>
+    </tr>
+    <?php if($filesRowNum > 0):$count = 0; foreach($files as $file): $count++;?>
+      <tr>
+        <td><?php echo $count; ?></td>
+        <td><a href="<?= $file['Files']['path'] . $file['Files']['name'] ?>"><?php echo $file['Files']['name']?></a></td>
+        <td><?php echo $file['Files']['created']; ?></td>
+        <td><?php echo $file['Files']['ip_address']; ?></td>
+      </tr>
+    <?php endforeach; else:?>
+      <tr><td colspan="4">No file(s) found......</td>
+    <?php endif; ?>
+  </table>
+</div>
